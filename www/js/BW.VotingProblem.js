7103d8a3071eed782982a10b7e816aa8e2feeedf
Narasimhan <snarasim@ctq.ndc.nasa.gov>
2015-07-20 15:46:10 -0700

    fixing size of bidding box

--- www/js/BW.VotingProblem.js
+++ www/js/BW.VotingProblem.js
@@ -695,7 +695,10 @@ BW.VotingProblem.prototype.show = function( data ) {
 		auction.toBiddingBox( this.bbConfig );
 		var screenWidth = $( window ).width();
 		if ( screenWidth > BW.maxScreenWidth ) screenWidth = BW.maxScreenWidth;
-		var scalingFactor = screenWidth * 1.0 / BW.unitWidth;	
+        screenWidth = screenWidth - 25;
+        alert(screenWidth);
+		var scalingFactor = screenWidth * 1.0 / BW.unitWidth;
+        alert(scalingFactor);
 		if ( scalingFactor > 1 ) {
 			var newWidth = $( ".bw-bidding-box-field" ).width() * scalingFactor;
 			var newFontSize = parseInt( $( ".bw-bidding-box-field" ).css( "font-size" ) ) * scalingFactor;
